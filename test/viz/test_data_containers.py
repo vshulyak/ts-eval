72c6123546ca7494bf242f072db1d584a7d568ed
Vladimir Shulyak <vladimir@shulyak.net>
2019-09-27 21:09:49 +0300

    Add data containers (+xarray factories)

---
+++ test/viz/test_data_containers.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+import pytest
+
+from ts_eval.viz.data_containers import xr_2d_factory, xr_3d_factory
+from ts_eval.viz.utils import time_align
+
+"""
+xarray format checks
+"""
+
+
+def test_xr_2d_factory__xarray_fmt(dataset_2d):
+
+    xarr = xr_2d_factory(dataset_2d)
+
+    assert np.allclose(xarr.mean_.values, time_align(dataset_2d), equal_nan=True)
+    assert xarr.sizes['dt'] == dataset_2d.shape[0] + dataset_2d.shape[1]
+    assert xarr.sizes['h'] == dataset_2d.shape[1]
+
+
+def test_xr_3d_factory__xarray_fmt(dataset_3d):
+
+    xarr = xr_3d_factory(dataset_3d)
+
+    assert np.allclose(xarr.upper.values, time_align(dataset_3d[:, :, 0]), equal_nan=True)
+    assert np.allclose(xarr.mean_.values, time_align(dataset_3d[:, :, 1]), equal_nan=True)
+    assert np.allclose(xarr.lower.values, time_align(dataset_3d[:, :, 2]), equal_nan=True)
+    assert xarr.sizes['dt'] == dataset_3d.shape[0] + dataset_3d.shape[1]
+    assert xarr.sizes['h'] == dataset_3d.shape[1]
+
+
+"""
+Input data checks
+"""
+
+
+def test_xr_2d_factory__nan(dataset_2d):
+    dataset_2d = dataset_2d.copy()
+    dataset_2d[:] = np.nan
+
+    with pytest.raises(AssertionError):
+        xr_2d_factory(dataset_2d)
+
+
+def test_xr_2d_factory__shape(dataset_3d):
+
+    # 3d array => 2d array
+    with pytest.raises(AssertionError):
+        xr_2d_factory(dataset_3d)
+
+
+def test_xr_3d_factory__nan(dataset_3d):
+    dataset_3d = dataset_3d.copy()
+    dataset_3d[:] = np.nan
+
+    with pytest.raises(AssertionError):
+        xr_3d_factory(dataset_3d)
+
+
+def test_xr_3d_factory__shape(dataset_2d):
+
+    # 2d array => 3d array
+    with pytest.raises(AssertionError):
+        xr_3d_factory(dataset_2d)
---
+++ ts_eval/viz/data_containers.py
@@ -0,0 +1,72 @@
+from collections import namedtuple
+from datetime import datetime
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+import xarray as xr
+
+from .utils import time_align
+
+MetricRes = namedtuple(
+    "MetricRes", ["overall", "overall_is_same", "steps", "steps_is_same"]
+)
+
+
+def xr_2d_factory(
+    arr: np.ndarray, start_date: Optional[datetime] = None, freq: Optional[str] = None
+) -> xr.Dataset:
+    """
+    Constructs a time aligned 2d-xarray
+    """
+    assert arr.ndim == 2, "Can work only with 2 dimentional arrays"
+    assert not np.isnan(arr).any(), "The input data can't contain NaNs"
+
+    n, h = arr.shape
+
+    ts_index = _gen_dataset_index(arr_len=n + h, start_date=start_date, freq=freq)
+
+    return xr.Dataset(
+        {
+            # indices
+            "dt": ts_index,
+            "h": list(range(h)),
+            # series
+            "mean_": (("dt", "h"), time_align(arr)),
+        }
+    )
+
+
+def xr_3d_factory(
+    arr: np.ndarray, start_date: Optional[datetime] = None, freq: Optional[str] = None
+) -> xr.Dataset:
+    """
+    Constructs a time aligned 3d-xarray
+    """
+    assert arr.ndim == 3, "Can work only with 3 dimentional arrays"
+    assert not np.isnan(arr).any(), "The input data can't contain NaNs"
+    assert arr.shape[2] == 3, "The 2nd axis has to have dim=3 (upper, mean, lower)"
+
+    n, h, _ = arr.shape
+
+    ts_index = _gen_dataset_index(arr_len=n + h, start_date=start_date, freq=freq)
+
+    return xr.Dataset(
+        {
+            # indices
+            "dt": ts_index,
+            "h": list(range(h)),
+            # series
+            "upper": (("dt", "h"), time_align(arr[:, :, 0])),
+            "mean_": (("dt", "h"), time_align(arr[:, :, 1])),
+            "lower": (("dt", "h"), time_align(arr[:, :, 2])),
+        }
+    )
+
+
+def _gen_dataset_index(arr_len, start_date, freq):
+    if start_date and freq:
+        return pd.date_range(start_date, freq=freq, periods=arr_len)
+    else:
+        return list(range(arr_len))
