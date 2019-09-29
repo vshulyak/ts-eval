4159d86ab7eff725dd6fe5b475f6e4481ae91074
Vladimir Shulyak <vladimir@shulyak.net>
2019-09-29 20:03:01 +0300

    Change metrics functions to have a name prop

--- ts_eval/viz/metrics/__init__.py
+++ ts_eval/viz/metrics/__init__.py
@@ -3,10 +3,20 @@ from functools import partial
 from .defs import ae, rMIS, se
 from .factories import absolute_metric, relative_metric
 
-MSE = partial(absolute_metric, metric_fn=se)
-MAE = partial(absolute_metric, metric_fn=ae)
-rMSE = partial(relative_metric, metric_fn=se)
-rMAE = partial(relative_metric, metric_fn=ae)
-FVrMSE = partial(relative_metric, metric_fn=se, fv=True)
-FVrMAE = partial(relative_metric, metric_fn=ae, fv=True)
-FVrMIS = partial(rMIS, fv=True)
+
+def partial_with_name(fn, name, **kwargs):
+    """
+    Creates a partials with a 'name' property
+    """
+    p = partial(fn, **kwargs)
+    p.name = name
+    return p
+
+
+MSE = partial_with_name(absolute_metric, "MSE", metric_fn=se)
+MAE = partial_with_name(absolute_metric, "MAE", metric_fn=ae)
+rMSE = partial_with_name(relative_metric, "rMSE", metric_fn=se)
+rMAE = partial_with_name(relative_metric, "rMAE", metric_fn=ae)
+FVrMSE = partial_with_name(relative_metric, "FVrMSE", metric_fn=se, fv=True)
+FVrMAE = partial_with_name(relative_metric, "FVrMAE", metric_fn=ae, fv=True)
+FVrMIS = partial_with_name(rMIS, "FVrMIS", fv=True)
