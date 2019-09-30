5e432bd82e1aa09f36fb8d9c8bc1c9e9a518bb60
Vladimir Shulyak <vladimir@shulyak.net>
2019-09-30 10:23:05 +0300

    Get rid of flit setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = [
+    "pandas>=0.23.0",
+    "numpy>=1.16.0",
+    "xarray>=0.13",
+    "scipy>=1.3",
+    "statsmodels>=0.10",
+    "jupyterlab>=1.1",
+    "matplotlib>=3.0",
+    "ipykernel",
+    "jupyter-contrib-nbextensions",
+]
+EXTRAS_REQUIRE = {"tests": ["hypothesis", "pytest"], "debug": ["pdbpp"]}
+EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["debug"]
+
+
+setuptools.setup(
+    name="ts-eval",
+    version="0.0.1",
+    author="Vladimir Shulyak",
+    author_email="vladimir@shulyak.net",
+    description="Time Series analysis and evaluation tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vshulyak/ts-eval",
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ ts_eval/__init__.py
@@ -0,0 +1,3 @@
+"""Time Series analysis and evaluation tools"""
+
+__version__ = "0.1"
