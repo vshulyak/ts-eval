4ff70d127fe1985ed21e25e856066093cbe17b45
Vladimir Shulyak <vladimir@shulyak.net>
2019-10-04 11:52:42 +0300

    Add changelog and twine

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+##  0.1.0 (2019-10-04)
+
+### Features
+
+- N-step ahead evaluation widget for Jupyter
+- Absolute & relative metrics for point forecasts and prediction intervals (MSE, MAE, rMSE, rMAE, MIS, rMIS)
+- Naive/Seasonal models for baseline (with prediction intervals)
+- Helper functions to evaluate n-step ahead forecasts using Statsmodels models or naive/seasonal naive models.
+- Holiday features generation and model evaluation on holiday datetimes.
+
+
+## 0.0.1 (2019-09-18)
+
+### Features
+
+- Fixed fourier series generation (fixed in time according to pandas index)
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import setuptools
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def read(fname):
+    with open(fname, "r") as fh:
+        return fh.read()
+
+
+LONG_DESCRIPTION = read("README.md") + read("CHANGELOG.md")
 
 INSTALL_REQUIRES = [
     "pandas>=0.23.0",
@@ -18,11 +22,13 @@ INSTALL_REQUIRES = [
 ]
 EXTRAS_REQUIRE = {
     "tests": ["hypothesis[numpy]", "pytest"],
+    "pypi": ["twine"],
     "debug": ["pdbpp"],
     "extra_runtime_libs": ["holidays>=0.9"],
 }
 EXTRAS_REQUIRE["dev"] = (
     EXTRAS_REQUIRE["tests"]
+    + EXTRAS_REQUIRE["pypi"]  # NOQA
     + EXTRAS_REQUIRE["debug"]  # NOQA
     + EXTRAS_REQUIRE["extra_runtime_libs"]  # NOQA
 )
@@ -30,11 +36,11 @@ EXTRAS_REQUIRE["dev"] = (
 
 setuptools.setup(
     name="ts-eval",
-    version="0.0.1",
+    version="0.1.0",
     author="Vladimir Shulyak",
     author_email="vladimir@shulyak.net",
     description="Time Series analysis and evaluation tools",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/vshulyak/ts-eval",
     packages=setuptools.find_packages(),
