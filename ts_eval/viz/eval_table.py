0edbf83ab9c4d0a7ed0cca41e4eee73623a3eae0
Vladimir Shulyak <vladimir@shulyak.net>
2019-10-14 17:47:12 +0300

    Add EvalTable mod of SimpleTable

---
+++ ts_eval/viz/eval_table.py
@@ -0,0 +1,61 @@
+from functools import partial
+
+from statsmodels.iolib.table import Cell, SimpleTable, pad
+
+
+FORMATTER_NAME = "callable_formatted"
+
+
+class TSEvalCell(Cell):
+    def format(self, width, output_format="txt", **fmt_dict):
+        if self.datatype == FORMATTER_NAME:
+            return self.format_string_formatted(
+                width, output_format=output_format, **fmt_dict
+            )
+        else:
+            return super().format(width, output_format=output_format, **fmt_dict)
+
+    def format_string_formatted(self, width, output_format="txt", **fmt_dict):
+        fmt = self._get_fmt(output_format, **fmt_dict)
+        data = self.data
+        datatype = self.datatype
+
+        assert callable(fmt[datatype]), "A callable should be provided as formatter"
+
+        content = fmt[datatype](data)
+        content = self._latex_escape(content, fmt, output_format)
+
+        align = self.alignment(output_format, **fmt)
+        return pad(content, width, align)
+
+
+def html_formatter(data):
+    assert len(data) == 4, "4-item tuple is expected as input"
+    value, color, warn_sign, is_text_bold = data
+
+    style = f"style='background: {color}'" if color else ""
+    warn_text = "*" if warn_sign else ""
+    text = (
+        f"<strong>{value}{warn_text}</strong>"
+        if is_text_bold
+        else f"{value}{warn_text}"
+    )
+
+    return f"<td {style}>{text}</td>"
+
+
+def txt_formatter(data):
+    assert len(data) == 4, "4-item tuple is expected as input"
+    value, _, warn_sign, _ = data
+
+    warn_text = "*" if warn_sign else ""
+    return f"{value}{warn_text}"
+
+
+EvalTable = partial(
+    SimpleTable,
+    datatypes=[FORMATTER_NAME],
+    html_fmt={FORMATTER_NAME: html_formatter},
+    txt_fmt={FORMATTER_NAME: txt_formatter},
+    celltype=TSEvalCell,
+)
